3676136b1d6afa170f8e16726fefcd4fb7cd53a3
claudio andres <claudioandreschacon@gmail.com>
2020-12-27 20:15:52 -0300

    estructura portafolio

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,22 @@
-$(function(){
-    $("a").click(function(event) {
-    if (this.hash !== "") {
-    event.preventDefault();
-    var gato = this.hash;
-    
-    $('html, body').animate({
-    scrollTop: $(gato).offset().top
-    }, 800, function(){
-    window.location.hash = gato;
+$(function () {
+    $("a").click(function (event) {
+        if (this.hash !== "") {
+            event.preventDefault();
+            var gato = this.hash;
+
+            $('html, body').animate({
+                scrollTop: $(gato).offset().top
+            }, 800, function () {
+                window.location.hash = gato;
+            });
+        }
     });
-    } 
-    });
-    });
+});
 
 $(function () {
     $('[data-toggle="tooltip"]').tooltip()
-    })
+})
 
-    $(function () {
-        $('[data-toggle="popover"]').popover()
-        })
+$(function () {
+    $('[data-toggle="popover"]').popover()
+})
